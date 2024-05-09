b068ef7c3d3476715185f2dcc45e2e7cee4798d3
achaney4 <achan014@odu.edu>
2024-05-09 18:00:21 -0400

    made more changes. added js file.

---
+++ affirm.js
@@ -0,0 +1,5 @@
+const affirmContainer = document.getElementById('affirm-container');
+const affirmText = document.getElementById('affirm');
+const newAffirmBtn = document.getElementById('new-affirm');
+
+let apiAffirm = [];

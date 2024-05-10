eb1f5e16bf423df241705ea70ee074f09a8c5aa0
achaney4 <achan014@odu.edu>
2024-05-09 22:12:09 -0400

    made more changes

--- affirm.js
+++ affirm.js
@@ -1,5 +1,50 @@
+
 const affirmContainer = document.getElementById('affirm-container');
 const affirmText = document.getElementById('affirm');
 const newAffirmBtn = document.getElementById('new-affirm');
 
+
 let apiAffirm = [];
+
+  
+
+  // Show New Quote
+function newAffirm() {
+    //loading();
+    // Pick a random quote from apiQuotes array
+    const affirmation = apiAffirm[Math.floor(Math.random() * apiAffirm.length)];
+    
+    affirmText.textContent = affirmation.text;
+    
+  }
+
+    // Get Quotes From API 
+    /*async function getAffirms() {
+        //loading();
+        const apiUrl = 'https://type.fit/api/quotes';
+        try {
+          const response = await fetch(apiUrl);
+          apiAffirm = await response.json();
+          newAffirm();
+        } catch (error) {
+          // Catch Error Here
+        }
+      }*/
+
+  // Get Quotes From API 
+    async function getAffirms() {
+        //loading();
+        const apiUrl = 'affirmApi.json';
+        try {
+          const response = await fetch(apiUrl);
+          apiAffirm = await response.json();
+          newAffirm();
+        } catch (error) {
+          // Catch Error Here
+        }
+      }
+
+
+getAffirms();
+
+ 
